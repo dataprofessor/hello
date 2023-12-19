a13aa9468cdd53d43f54080318573631c69faa48
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-12-19 18:46:46 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,24 +9,38 @@ st.title('Welcome to Streamlit 👋')
 
 st.write("I was built with # lines of code. It's wicked easy")
 with st.expander('Expand to see my code'):
-  st.code('''
+  code = '''
   import streamlit as st
 
   st.write('Hello world!')
-  ''')
+  '''
+  st.code(code)
 
 st.header('What would you like to build?', divider=True)
 
 col = st.columns(4)
 
 with col[0]:
-  st.button('**Your Streamlit AI Assistant**')
+  btn1 = st.button('**Your Streamlit AI Assistant**')
 with col[1]:
-  st.button('**Your LLM Playground**')
+  btn2 = st.button('**Your LLM Playground**')
 with col[2]:
-  st.button('**Exploratory Data Analysis**')
+  btn3 = st.button('**Exploratory Data Analysis**')
 with col[3]:
-  st.button('**Build your ML Model**')
+  btn4 = st.button('**Build your ML Model**')
+
+if btn1:
+  col1 = st.columns(2)
+  with col1[0]:
+    app1 = 'https://builder.streamlit.app'
+    st.components.v1.html(f"""<iframe src="{app1}/?embed=true" height=350 style="width:100%;border:none;"></iframe>""", width=None, height=1200, scrolling=False)
+  with col1[1]:
+    code1 =
+    st.code(code)
+
+
+
+
 
 
 

ad82a5c5e4251a15fb6dd4d03b0abc843b626e8c
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-12-20 00:14:20 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -69,4 +69,4 @@ if st.session_state.page == 'Page 1':
       #st.code(read_gh('https://raw.githubusercontent.com/dataprofessor/builder/master/streamlit_app.py'), line_numbers=True)
   with col1[1]:
     app1 = 'https://builder.streamlit.app'
-    st.components.v1.html(f"""<iframe src="{app1}/?embed=true" height=380 style="width:100%;border:none;"></iframe>""", width=None, scrolling=False)
+    st.components.v1.html(f"""<iframe src="{app1}/?embed=true" height=380 style="width:100%;border:none;"></iframe>""", width=None, height=None, scrolling=False)

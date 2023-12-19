2574c58091c25302d163a0c3199372a61874ad4a
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-12-19 19:57:48 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,7 @@ st.markdown("""
     padding-top: 1.3rem;
     padding-bottom: 0rem;
 
-.element-container.st-emotion-cache-1clmrd1.e1f1d6gn4 {
+.stCodeBlock.st-emotion-cache-12r09dv.e1ycw9pz1 {
     height: 380px;
 }
 

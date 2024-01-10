901ab97592db8ea0c030fa3b77bdc9182cbf131b
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2024-01-10 19:00:46 +0700

    Update css.py

--- css.py
+++ css.py
@@ -20,7 +20,7 @@ def load_css():
   # Social icons https://codepen.io/kpdushanmaduka/pen/jadeoO
   css_styles = '''
   i {
-    color: #111111;
+    color: #616A6B;
     letter-spacing: 9px;
     padding: 0.5cm 0cm 0.5cm 0cm;
     font-size: 20px;

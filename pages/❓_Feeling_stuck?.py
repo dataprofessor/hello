ef3507c4f5d268414ab417fd1e8dd95e084bb1e1
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2024-01-10 18:27:31 +0700

    Update ❓_Feeling_stuck?.py

--- pages/❓_Feeling_stuck?.py
+++ pages/❓_Feeling_stuck?.py
@@ -1,2 +1,12 @@
 import streamlit as st
 
+# Set page configuration
+st.set_page_config(
+    page_title='Feeling stuck?',
+    page_icon='❓',
+)
+
+# Display title
+st.title('❓ Feeling stuck?')
+
+st.write('Work in progress ...')
